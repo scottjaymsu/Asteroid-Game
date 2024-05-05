fa2c322b2b8519736db3a0ff11f7fac4141b0457
Jay <scottjay@msu.edu>
2024-05-05 00:35:27 -0400

    Ship UI element is created and added as child element to Pane object

--- AstroidApp/src/main/java/application/astroidapp/GameWindow.java
+++ AstroidApp/src/main/java/application/astroidapp/GameWindow.java
@@ -3,22 +3,44 @@ package application.astroidapp;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
+/**
+ * GameWindow Class
+ */
 public class GameWindow extends Application {
-
+    /**
+     * Automatically called when JavaFX application is launched.
+     * Sets up the initial stage with a Pane and a Scene for the game.
+     * @param stage The primary stage that is constructed by the platform.
+     * @throws Exception If an error occurs during initialization an exception is thrown.
+     */
     @Override
     public void start(Stage stage) throws Exception {
+        // Pane layout arranges the nodes in the application
         Pane pane = new Pane();
-        pane.setPrefSize(300, 200);
-        pane.getChildren().add(new Circle(30, 50, 10));
+        pane.setPrefSize(600, 400);
 
+        // Polygon object is used to draw ship
+        Polygon ship = new Polygon(-5, -5, 10, 0, -5, 5);
+        ship.setTranslateX(300);
+        ship.setTranslateY(200);
+
+        // UI element is added to Pane container as child node
+        pane.getChildren().add(ship);
+
+        // Scene is specified and the pane is used as the root
         Scene scene = new Scene(pane);
+        stage.setTitle("Asteroids!");
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * Main entry for program
+     * @param args
+     */
     public static void main(String[] args) {
         launch(args);
     }
